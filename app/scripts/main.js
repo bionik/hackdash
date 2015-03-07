cce18677b8babfac768b2960f5ca0cdb43f29499
Lauri Hakkarainen <late@Muikku.local>
2015-03-07 17:53:43 +0200

    Socket stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,15 @@
 //Made by @b10nik
 
 //Linter preferences
-/*global $, moment, Chart*/
+/*global $, moment, Chart, io*/
 /*jslint node: true*/
 
 var config = {
   debug: true,
-  temperatureLabels: ['0h', '', '', '', '1h', '', '', '', '2h', '', '', '', '3h']
+  apiLocation: 'http://192.168.11.2/pi_api/',
+  maxRows: 30,
+  color: '254,254,254',
+  temperatureLabels: ['0h', '', '', '', '1h', '', '', '', '2h', '', '', '', '3h'],
 };
 
 function log(obj){
@@ -57,6 +60,37 @@ var App = function(container){
     }
   };
 
+  a.addMessage = function(data){
+    log('addMessage');
+    var dom = '';
+    var source, template;
+
+    if (data.type === 'message'){
+      source = $('#content-mesage-template').html();
+      template = window.Handlebars.compile(source);
+      dom = $(template(data.data));
+
+    } else if (data.type === 'notification') {
+      source = $('#content-notification-template').html();
+      template = window.Handlebars.compile(source);
+      dom = $(template(data.data));
+
+    } else if (data.type === 'status') {
+      source = $('#content-status-template').html();
+      template = window.Handlebars.compile(source);
+      dom = $(template(data.data));
+    }
+
+    $('#content-irc').append(dom);
+    $(dom).slideDown(400);
+
+    while($('#content-irc').find('.row').length > config.maxRows){
+      log('Over '+config.maxRows+' messages, delete first row');
+      $('#content-irc').find('.row').first().remove();
+    }
+
+  };
+
   a.addTemperature = function(reading){
     log('addTemperature');
     if (a.temperatureGraph.datasets[0].points.length > 12) {
@@ -76,9 +110,9 @@ var App = function(container){
 
   };
 
-  a.updateLights = function(room1, room2){
-    log('updateLights');
-    $('#room1','#room2').attr('class', '');
+  a.refreshLights = function(room1, room2){
+    log('refreshLights');
+    $('#room1, #room2').removeClass('on off');
     if(room1){
       $('#room1').html('on').addClass('on');
     } else {
@@ -93,85 +127,128 @@ var App = function(container){
 
   //Update every second
   a.updateSecond = function(){
-    log('updateSecond');
+    //log('updateSecond');
     $(a.timeContainer).html(moment().format('HH:mm'));
   };
 
   a.updateMinute = function(){
-    log('updateMinute');
+    //log('updateMinute');
     $(a.dateContainer).html(moment().format('dddd, MMMM Do').toLowerCase());
 
-    a.updateLights(true, false);
+    //Get light status and update.
+    a.updateLights();
   };
 
   a.update15Minutes = function(){
-    log('update15Minutes');
+    //log('update15Minutes');
+
     //Get temperature and update it in the graph.
-    a.addTemperature( Math.floor(Math.random()*40-20) );
+    a.updateTemperature();
+  };
+
+  a.updateLights = function(){
+    log('updateLights');
+    var room1, room2;
+
+    $.when(
+      $.get(config.apiLocation+'gpio/', {a: 'readPin', pin: 1}, function(response){
+        log(response);
+        room1 = parseInt(response.data, 10);
+      }, 'JSON'),
+      $.get(config.apiLocation+'gpio/', {a: 'readPin', pin: 0}, function(response){
+        log(response);
+        room2 = parseInt(response.data, 10);
+      }, 'JSON')
+    ).then(function(){
+      a.refreshLights(room1 === 0, room2 === 0);
+    });
+  };
+
+  a.updateTemperature = function(){
+    $.get(config.apiLocation+'temp/', {a: 'getTemp'}, function(response){
+      log(response);
+      a.addTemperature( response.data );
+    }, 'JSON');
   };
 
   a.init = function(){
     log('init');
-    var color = '254,254,254';
 
-    Chart.defaults.global.scaleFontColor = 'rgba(251,251,251,1)';
+    a.io = io('http://192.168.11.2:8008');
+    a.io.on('packet', function(data) {
+      log(data);
+      if(typeof data.type !== 'undefined'){
+        a.addMessage(data);
+      }
+    });
+
+    Chart.defaults.global.scaleFontColor = 'rgba('+config.color+',1)';
     Chart.defaults.global.animationSteps = 30;
 
-    var firstTemperature = 17;
+    var firstTemperature;
 
-    var data = {
-      labels: ['',''],
-      datasets: [
-        {
-          label: 'Temperature',
-          fillColor: 'rgba('+color+',0.1)',
-          strokeColor: 'rgba('+color+',1)',
-          pointColor: 'rgba('+color+',1)',
-          pointStrokeColor: 'rgba('+color+',0.8)',
-          pointHighlightFill: 'rgba('+color+',1)',
-          pointHighlightStroke: 'rgba('+color+',1)',
-          data: [firstTemperature, firstTemperature]
-        }
-      ]
-    };
+    $.when(
+      $.get(config.apiLocation+'temp/', {a: 'getTemp'}, function(response){
+        firstTemperature = response.data;
+      }, 'JSON')
 
-    var options = {
-      scaleShowGridLines : true,
-      scaleGridLineColor : 'rgba(251,251,251,0.1)',
-      scaleGridLineWidth : 1,
-      scaleShowHorizontalLines: true,
-      scaleShowVerticalLines: false,
-      bezierCurve : true,
-      bezierCurveTension : 0,
-      pointDot : true,
-      pointDotRadius : 3,
-      pointDotStrokeWidth : 1,
-      pointHitDetectionRadius : 20,
-      datasetStroke : true,
-      datasetStrokeWidth : 2,
-      datasetFill : true,
-      legendTemplate : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<datasets.length; i++){%><li><span style=\'background-color:<%=datasets[i].strokeColor%>\'></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
-    };
+    ).then(function(){
 
-    //Get chart canvas & context
-    var ctx = document.getElementById('graph_temperature').getContext('2d');
-    a.temperatureGraph = new Chart(ctx).Line(data, options);
+      var data = {
+        labels: ['',''],
+        datasets: [
+          {
+            label: 'Temperature',
+            fillColor: 'rgba('+config.color+',0.1)',
+            strokeColor: 'rgba('+config.color+',1)',
+            pointColor: 'rgba('+config.color+',1)',
+            pointStrokeColor: 'rgba('+config.color+',0.8)',
+            pointHighlightFill: 'rgba('+config.color+',1)',
+            pointHighlightStroke: 'rgba('+config.color+',1)',
+            data: [firstTemperature, firstTemperature]
+          }
+        ]
+      };
 
-    //Refresh screen size
-    a.refreshSize();
+      var options = {
+        scaleShowGridLines : true,
+        scaleGridLineColor : 'rgba('+config.color+',0.1)',
+        scaleGridLineWidth : 1,
+        scaleShowHorizontalLines: true,
+        scaleShowVerticalLines: false,
+        bezierCurve : true,
+        bezierCurveTension : 0,
+        pointDot : true,
+        pointDotRadius : 3,
+        pointDotStrokeWidth : 1,
+        pointHitDetectionRadius : 20,
+        datasetStroke : true,
+        datasetStrokeWidth : 2,
+        datasetFill : true,
+        legendTemplate : '<ul class=\'<%=name.toLowerCase()%>-legend\'><% for (var i=0; i<datasets.length; i++){%><li><span style=\'background-color:<%=datasets[i].strokeColor%>\'></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
+      };
 
-    //Refresh screen size on resize
-    $(window).resize(a.refreshSize);
+      //Get chart canvas & context
+      var ctx = document.getElementById('graph_temperature').getContext('2d');
+      a.temperatureGraph = new Chart(ctx).Line(data, options);
 
-    //Run timed events for the first time
-    a.updateSecond();
-    a.updateMinute();
-    a.update15Minutes();
+      //Refresh screen size
+      a.refreshSize();
 
-    //Start timer
-    this.timer = window.setInterval(function(){
-      a.tick();
-    }, 3000);
+      //Refresh screen size on resize
+      $(window).resize(a.refreshSize);
+
+      //Run timed events for the first time
+      a.updateSecond();
+      a.updateMinute();
+      a.update15Minutes();
+
+      //Start timer
+      this.timer = window.setInterval(function(){
+        a.tick();
+      }, 1000);
+
+    });
 
   };
 
